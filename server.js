56fe5ded7ba205d25fdff149f4eb63ecaea33034
AjeetSln <ajeet2215617@keshav.du.ac.in>
2025-01-16 10:13:11 +0530

    first commit

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ if (cluster.isMaster) {
   });
 } else {
   // Server setup for each worker process
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 5001;
   
   // Socket.IO communication
   io.on('connection', (socket) => {
